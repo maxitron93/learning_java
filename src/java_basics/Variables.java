3a17e7f6bbe737647f9ca930f378259a138a80eb
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-12-09 11:01:04 +1100

    First commit

---
+++ src/java_basics/Variables.java
@@ -0,0 +1,24 @@
+package java_basics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        // Declaring variables
+        int y;
+        String words;
+
+        // Assigning variables
+        y = 10;
+        words = "This is another sentence";
+
+        // Combining declaration and assignment
+        int x = 4;
+        String sent = "This is a sentence";
+
+        // Printing output
+        System.out.println(x + y);
+        System.out.println(sent);
+        System.out.println(words);
+    }
+
+}
